7aa949af4afc44f4a7ff37cfc608e71594a6e104
sreeja126 <nainaboinasreeja74@gmail.com>
2025-10-27 19:34:27 +0530

    Added frontend and connected backend successfully

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -24,7 +24,7 @@ const Signin = () => {
         <div className="flex space-x-3">
           <button onClick={async()=>{
             try {
-              const response = await axios.post("http://localhost:3000/api/v1/user/signin", {
+              const response = await axios.post("https://my-backend-9hhv.onrender.com/api/v1/user/signin", {
               username,
               password,
               });
